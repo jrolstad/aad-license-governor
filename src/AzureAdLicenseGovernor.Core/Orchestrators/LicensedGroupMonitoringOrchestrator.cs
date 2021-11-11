33255821c8a549c45a9a8a819b3251afc1cb2f6a
Josh Rolstad <jorolsta@microsoft.com>
2021-11-10 19:41:05 -0800

    Adding monitoring

--- src/AzureAdLicenseGovernor.Core/Orchestrators/LicensedGroupMonitoringOrchestrator.cs
+++ src/AzureAdLicenseGovernor.Core/Orchestrators/LicensedGroupMonitoringOrchestrator.cs
@@ -1,10 +1,7 @@
 ﻿using AzureAdLicenseGovernor.Core.Models;
 using AzureAdLicenseGovernor.Core.Services;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AzureAdLicenseGovernor.Core.Orchestrators
