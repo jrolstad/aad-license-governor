2d27215e0d1ccd06a0c2c42e3e7c28f3ec10421f
Josh Rolstad <jrolstad@gmail.com>
2021-12-11 11:42:11 -0800

    Adding ability to get user data in batches

---
+++ src/AzureAdLicenseGovernor.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureAdLicenseGovernor.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int size)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / size)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+}
--- src/AzureAdLicenseGovernor.Core/Services/UserService.cs
+++ src/AzureAdLicenseGovernor.Core/Services/UserService.cs
@@ -1,8 +1,9 @@
-﻿using AzureAdLicenseGovernor.Core.Factories;
+﻿using AzureAdLicenseGovernor.Core.Extensions;
+using AzureAdLicenseGovernor.Core.Factories;
 using AzureAdLicenseGovernor.Core.Mappers;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AzureAdLicenseGovernor.Core.Services
@@ -20,7 +21,62 @@ namespace AzureAdLicenseGovernor.Core.Services
 
         public async Task<ICollection<Models.User>> GetUserLicenseAssignmentStates(Models.Directory directory, IEnumerable<string> userIds)
         {
-            return new List<Models.User>();
+            var client = await _graphClientFactory.CreateAsync(directory);
+
+            var requests = userIds
+                .Where(id => !string.IsNullOrWhiteSpace(id))
+                .Distinct(StringComparer.OrdinalIgnoreCase)
+                .Select(id => client.Users[id]
+                                    .Request()
+                                    .Select("id,userPrincipalName,licenseAssignmentStates")
+                                    );
+
+            var userData = await ExecuteBatch<Microsoft.Graph.User>(client, requests);
+
+            var result = userData
+                .AsParallel()
+                .Select(u => _userMapper.Map(directory, u))
+                .ToList();
+
+            return result;
+        }
+
+        public static async Task<IEnumerable<TResponse>> ExecuteBatch<TResponse>(Microsoft.Graph.IGraphServiceClient client,
+           IEnumerable<Microsoft.Graph.IBaseRequest> requests)
+        {
+            const int maximumBatchSize = 20; // Microsoft Graph API Batch Endpoint has a maximum of 20 requests in a single batch request
+            var groupedRequests = requests.Split(maximumBatchSize);
+
+            var resultTasks = groupedRequests
+                .Select(group => ExecuteBatchRequest<TResponse>(client, CreateBatchRequest(group)));
+            var resultData = await Task.WhenAll(resultTasks);
+            var results = resultData.SelectMany(r => r);
+
+            return results;
+        }
+
+        private static Microsoft.Graph.BatchRequestContent CreateBatchRequest(IEnumerable<Microsoft.Graph.IBaseRequest> requests)
+        {
+            var batchedRequest = new Microsoft.Graph.BatchRequestContent();
+
+            foreach (var request in requests)
+            {
+                batchedRequest.AddBatchRequestStep(request);
+            }
+
+            return batchedRequest;
+        }
+
+        private static async Task<IEnumerable<TResponse>> ExecuteBatchRequest<TResponse>(Microsoft.Graph.IGraphServiceClient client, Microsoft.Graph.BatchRequestContent batchedRequest)
+        {
+            var batchResponse = await client.Batch.Request().PostAsync(batchedRequest);
+            var responses = await batchResponse.GetResponsesAsync();
+
+            var resultTasks = responses
+                .Select(r => batchResponse.GetResponseByIdAsync<TResponse>(r.Key));
+            var result = await Task.WhenAll(resultTasks);
+
+            return result;
         }
     }
 }
