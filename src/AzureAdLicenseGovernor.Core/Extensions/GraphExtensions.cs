974bb3a8004a77d4dbe25898f52d180ee3de51fe
Josh Rolstad <jrolstad@gmail.com>
2021-12-11 11:46:51 -0800

    Extracting batch calls out to extension methods so it can be reused.

--- src/AzureAdLicenseGovernor.Core/Extensions/GraphExtensions.cs
+++ src/AzureAdLicenseGovernor.Core/Extensions/GraphExtensions.cs
@@ -1,10 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AzureAdLicenseGovernor.Core.Extensions
 {
     public static class GraphExtensions
     {
+        public static async Task<IEnumerable<TResponse>> ExecuteBatch<TResponse>(this Microsoft.Graph.IGraphServiceClient client,
+          IEnumerable<Microsoft.Graph.IBaseRequest> requests)
+        {
+            const int maximumBatchSize = 20; // Microsoft Graph API Batch Endpoint has a maximum of 20 requests in a single batch request
+            var groupedRequests = requests.Split(maximumBatchSize);
+
+            var resultTasks = groupedRequests
+                .Select(group => ExecuteBatchRequest<TResponse>(client, CreateBatchRequest(group)));
+            var resultData = await Task.WhenAll(resultTasks);
+            var results = resultData.SelectMany(r => r);
+
+            return results;
+        }
+
+        private static Microsoft.Graph.BatchRequestContent CreateBatchRequest(IEnumerable<Microsoft.Graph.IBaseRequest> requests)
+        {
+            var batchedRequest = new Microsoft.Graph.BatchRequestContent();
+
+            foreach (var request in requests)
+            {
+                batchedRequest.AddBatchRequestStep(request);
+            }
+
+            return batchedRequest;
+        }
+
+        private static async Task<IEnumerable<TResponse>> ExecuteBatchRequest<TResponse>(Microsoft.Graph.IGraphServiceClient client, Microsoft.Graph.BatchRequestContent batchedRequest)
+        {
+            var batchResponse = await client.Batch.Request().PostAsync(batchedRequest);
+            var responses = await batchResponse.GetResponsesAsync();
+
+            var resultTasks = responses
+                .Select(r => batchResponse.GetResponseByIdAsync<TResponse>(r.Key));
+            var result = await Task.WhenAll(resultTasks);
+
+            return result;
+        }
     }
 }
--- src/AzureAdLicenseGovernor.Core/Services/UserService.cs
+++ src/AzureAdLicenseGovernor.Core/Services/UserService.cs
@@ -31,50 +31,12 @@ namespace AzureAdLicenseGovernor.Core.Services
                                     .Select("id,userPrincipalName,licenseAssignmentStates")
                                     );
 
-            var userData = await ExecuteBatch<Microsoft.Graph.User>(client, requests);
+            var userData = await client.ExecuteBatch<Microsoft.Graph.User>(requests);
 
             var result = userData
                 .AsParallel()
                 .Select(u => _userMapper.Map(directory, u))
                 .ToList();
-
-            return result;
-        }
-
-        public static async Task<IEnumerable<TResponse>> ExecuteBatch<TResponse>(Microsoft.Graph.IGraphServiceClient client,
-           IEnumerable<Microsoft.Graph.IBaseRequest> requests)
-        {
-            const int maximumBatchSize = 20; // Microsoft Graph API Batch Endpoint has a maximum of 20 requests in a single batch request
-            var groupedRequests = requests.Split(maximumBatchSize);
-
-            var resultTasks = groupedRequests
-                .Select(group => ExecuteBatchRequest<TResponse>(client, CreateBatchRequest(group)));
-            var resultData = await Task.WhenAll(resultTasks);
-            var results = resultData.SelectMany(r => r);
-
-            return results;
-        }
-
-        private static Microsoft.Graph.BatchRequestContent CreateBatchRequest(IEnumerable<Microsoft.Graph.IBaseRequest> requests)
-        {
-            var batchedRequest = new Microsoft.Graph.BatchRequestContent();
-
-            foreach (var request in requests)
-            {
-                batchedRequest.AddBatchRequestStep(request);
-            }
-
-            return batchedRequest;
-        }
-
-        private static async Task<IEnumerable<TResponse>> ExecuteBatchRequest<TResponse>(Microsoft.Graph.IGraphServiceClient client, Microsoft.Graph.BatchRequestContent batchedRequest)
-        {
-            var batchResponse = await client.Batch.Request().PostAsync(batchedRequest);
-            var responses = await batchResponse.GetResponsesAsync();
-
-            var resultTasks = responses
-                .Select(r => batchResponse.GetResponseByIdAsync<TResponse>(r.Key));
-            var result = await Task.WhenAll(resultTasks);
 
             return result;
         }
